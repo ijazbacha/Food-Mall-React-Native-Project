0f4cde33b7b25eebe0dccc1bc0984031548458b6
Ijaz Bacha <76259605+bacha-11@users.noreply.github.com>
2021-11-10 17:55:06 +0500

    create header buttons

--- Components/HeaderBtns.js
+++ Components/HeaderBtns.js
@@ -1,12 +1,28 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import { Text, TouchableOpacity, View } from "react-native";
 
-
-export default function  HeaderBtns(){
-    return(
-        <View>
-            <Text>Delivery</Text>
-            <Text>PickUp</Text>
-        </View>
-    )
+export default function HeaderBtns() {
+  const [activeTab, setActiveTab] = useState('Delivery')
+  return (
+    <View style={{ flexDirection: "row", justifyContent: "center"  }}>
+      <CustomBtn text="Delivery" activeTab={activeTab} setActiveTab={setActiveTab}/>
+      <CustomBtn text="PickUp" activeTab={activeTab} setActiveTab={setActiveTab} />
+    </View>
+  );
 }
+
+const CustomBtn = ({ text, bgColor, tColor, activeTab, setActiveTab, ...props }) => {
+  
+  return (
+    <TouchableOpacity onPress={() => setActiveTab(text)}
+      style={{
+        backgroundColor:  activeTab === text ? 'black' : "white" ,
+        paddingVertical: 8,
+        paddingHorizontal: 12,
+        borderRadius: 20,
+      }}
+    >
+      <Text style={{ color: activeTab === text ? 'white' : "black" , fontWeight: "700" }}>{text}</Text>
+    </TouchableOpacity>
+  );
+};
